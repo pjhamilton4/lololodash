3a07f31cc67288a3b8037b33ce82ed98e5cd68c2
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-21 02:22:08 -0400

    3 of 10

--- program.js
+++ program.js
@@ -1,7 +1,16 @@
 const _ = require('lodash');
+const { toArray } = require('lodash');
 
 const worker = function(list){
-    return _.sortBy(list, 'quantity').reverse();
+  return _.forEach(list, function(element) {
+    if (element.population > 1) {
+      element.size = "big";
+    } else if(element.population > 0.5) {
+      element.size = "med";
+    } else {
+      element.size = "small";
+    }
+  });
 };
 
 module.exports = worker;
