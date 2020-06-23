1ada40d205c9a4e8b88865d1a98136faecb02e65
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-23 16:12:35 -0400

    9 of 9

--- program.js
+++ program.js
@@ -2,18 +2,8 @@ const _ = require('lodash');
 const { result, over } = require('lodash');
 require('lodash');
 
-const worker = function (freelancers) {
-    let average = _.meanBy(freelancers, (f) => f.income);
-
-    let underperform = [];
-    let overperform = [];
-
-    _.filter(freelancers, (f) => (f.income <= average ? underperform.push(f): overperform.push(f)));
-
-    underperform = _.sortBy(underperform, (f)=>f.income);
-    overperform = _.sortBy(overperform, (f)=>f.income);
-
-    return {average, underperform, overperform};
+const worker = function (data) {
+    return _.template('Hello <%= name %> (logins: <%= login.length %>)')(data);
 };
 
 
