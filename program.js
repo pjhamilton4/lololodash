1d434f9105d4cdaa36d246962cb947cae4e0ffb2
Philip Hamilton <pjhamilton4@gmail.com>
2020-06-22 21:24:11 -0400

    5 of 10

--- program.js
+++ program.js
@@ -2,22 +2,11 @@ const _ = require('lodash');
 const { toArray } = require('lodash');
 
 const worker = function (item) {
-    let result = {
-        warm: [],
-        hot: []
-    }
-
-    const check_temp = (item) => item > 19;
-
-    _.forEach(item, function (town, townname) {
-        if (_.every(town, check_temp)) {
-            result.hot.push(townname);
-        } else if (_.some(town, check_temp)) {
-            result.warm.push(townname);
-        }
-
-    });
-    return result;
+    let chainedWords = _.chain(item).map(function(word){
+        word = word + "chained"; 
+        return word.toUpperCase();
+    }).sortBy();
+    return chainedWords;
 };
 
 
